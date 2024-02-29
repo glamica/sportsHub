99572f4cb9e84c2cb304386fcb9f9fb1522f81fb
glamica <161377388+glamica@users.noreply.github.com>
2024-02-29 15:00:57 -0300

    Create consulta1.php

---
+++ consulta1.php
@@ -0,0 +1,13 @@
+<php?
+if (isset($_POST['cosultar'])) {
+  $con = mysqli_connect('locahost','root','','3pp');
+  $codigo = $_POST['codigo'];
+  $query = "SELECT * FROM produto WHERE codigo='$codigo'";
+  $return = mysqli_query($con,$query);
+  if ($result) {
+    while ($linha = mysqli_fetch_array($return)) {
+      echo $linha['descricao'].'<br>'
+      echo $linha['valor'].'<br> <hr.';
+  }
+}
+?>
